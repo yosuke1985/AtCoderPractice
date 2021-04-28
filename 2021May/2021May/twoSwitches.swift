256512113da116485b049e7d6c5d55854dc5d07c
Yosuke Nakayama <ihatenonoildressing@gmail.com>
2021-04-28 10:39:01 +0900

    update

--- 2021May/2021May/twoSwitches.swift
+++ 2021May/2021May/twoSwitches.swift
@@ -10,6 +10,63 @@ import Foundation
 
 func twoSwitches() {
     let items = readLine()!.split(separator: " ").map { Int($0)! }
+//    print(A,B,C,D)
+
+    let A = items[0]
+    let B = items[1]
+    let C = items[2]
+    let D = items[3]
+    var min = 0
+    var max = 0
     
-    
+    if C >= A {
+        if A...B ~= C {
+
+            min = C
+            if B >= D {
+
+                max = D
+            } else {
+                max = B
+            }
+
+            print(max-min)
+        } else {
+            print(0)
+        }
+    } else {
+        if C...D ~= A {
+            min = A
+            if B >= D {
+                max = D
+            } else {
+                max = B
+            }
+            print(max-min)
+        } else {
+            print(0)
+
+        }
+    }
 }
+
+//twoSwitches()
+
+func twoSwitches2() {
+    let items = readLine()!.split(separator: " ").map { Int($0)! }
+//    print(A,B,C,D)
+
+    let A = items[0]
+    let B = items[1]
+    let C = items[2]
+    let D = items[3]
+    
+
+    let low = max(A, C)
+    let high = min(B, D)
+    
+    print(max(0, high - low))
+}
+
+//twoSwitches2()
+
