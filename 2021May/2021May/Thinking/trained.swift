0b69bb35884e59f46dc3ed2aabb7888c8fdf45f6
Yosuke Nakayama <ihatenonoildressing@gmail.com>
2021-05-03 10:58:45 +0900

    update

--- 2021May/2021May/Thinking/trained.swift
+++ 2021May/2021May/Thinking/trained.swift
@@ -11,8 +11,32 @@ func trained() {
     let N = Int(readLine()!)!
     let numbers: [Int] = (0..<N).map { _ in Int(readLine()!)! }
     
-    print("N", N)
-    print("numbers", numbers)
+//    print("N", N)
+//    print("numbers", numbers)
+        
+    var countNum = 0
+    var found = false
+    
+    func function(number: Int) {
+        guard countNum < numbers.count else { return }
+        countNum += 1
+        let selectedNum = numbers[number - 1]
+
+        if selectedNum == 2 {
+            print(countNum)
+            found = true
+        } else{
+            function(number:selectedNum)
+        }
+    }
+    
+    function(number: 1)
+
+    if !found {
+        print(-1)
+    }
 }
 
 //trained()
+
+// 再帰はつかわず、フラグ使わずにもっとシンプルにかける。
