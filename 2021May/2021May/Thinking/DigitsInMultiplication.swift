8ab66a890ee6c48a9d3840595f685ec047a34289
Yosuke Nakayama <ihatenonoildressing@gmail.com>
2021-06-04 15:39:53 +0900

    update

---
+++ 2021May/2021May/Thinking/DigitsInMultiplication.swift
@@ -0,0 +1,19 @@
+//
+//  DigitsInMultiplication.swift
+//  2021June
+//
+//  Created by Yosuke Nakayama on 2021/06/04.
+//
+
+import Foundation
+
+
+func DigitsInMultiplication() {
+    
+    let N = Int(readLine()!)!
+    
+
+    
+}
+
+//DigitsInMultiplication()
