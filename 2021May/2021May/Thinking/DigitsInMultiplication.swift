67f52fac6c60023c19579255b6a7f60d66a9cb07
Yosuke Nakayama <ihatenonoildressing@gmail.com>
2021-06-05 10:39:42 +0900

    update

--- 2021May/2021May/Thinking/DigitsInMultiplication.swift
+++ 2021May/2021May/Thinking/DigitsInMultiplication.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Yosuke Nakayama on 2021/06/04.
 //
+// https://atcoder.jp/contests/abc057/tasks/abc057_c
 
 import Foundation
 
@@ -11,9 +12,26 @@ import Foundation
 func DigitsInMultiplication() {
     
     let N = Int(readLine()!)!
+    var divisorList = Set<Int>()
+    var minLog10List = Set<Int>()
+    
+    for i in 1...N where N % i == 0 {
+        divisorList.insert(i)
+    }
     
 
-    
+    for i in divisorList.sorted() {
+        let division = N / i
+
+        let iLog10 = Int(ceil(log10(Double(i))))
+        let divLog10 = Int(ceil(log10(Double(division))))
+        
+        let minLog10 = iLog10 > divLog10 ? iLog10: divLog10
+        minLog10List.insert(minLog10)
+
+    }
+    print(minLog10List.min()! + 1)
+
 }
 
 //DigitsInMultiplication()
---
+++ 2021May/2021May/Thinking/halfAndHalf.swift
@@ -0,0 +1,16 @@
+//
+//  halfAndHalf.swift
+//  2021June
+//
+//  Created by Yosuke Nakayama on 2021/06/05.
+//
+// https://atcoder.jp/contests/abc095/tasks/arc096_a
+
+import Foundation
+
+
+func halfAndHalf() {
+    
+}
+
+//halfAndHalf()
