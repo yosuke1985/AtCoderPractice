d62c252dc6e20d428bad6a02eed4219d95423d50
Yosuke Nakayama <ihatenonoildressing@gmail.com>
2021-06-28 14:50:55 +0900

    update

---
+++ 2021June/2021June/CA/aDiv.swift
@@ -0,0 +1,26 @@
+//
+//  aDiv.swift
+//  2021June
+//
+//  Created by Yosuke Nakayama on 2021/06/28.
+//
+
+import Foundation
+
+func aDiv () {
+    let N = Int(readLine()!)!
+    
+    print(N-1)
+}
+
+//aDiv()
+
+
+/*
+ 1 0
+ 2 1 (1,1)
+ 3,2 (1,2), (2,1)
+ 4,3 (1,3), (2,2), (3,1)
+ 5,4 (1,4), (2,3), (3,2), (4,1)
+
+ */
---
+++ 2021May/2021May/Thinking/main.swift
@@ -0,0 +1,41 @@
+//
+//  Palindrome0.swift
+//  2021June
+//
+//  Created by Yosuke Nakayama on 2021/06/28.
+//
+
+import Foundation
+
+
+func Palindrome0() {
+    let N = Int(readLine()!)!
+    
+    var checkList = Array(String(N))
+    
+//    print("checkList", checkList)
+//    print(type(of: String(checkList)))
+//    print("String(checkList)", String(checkList))
+    
+    guard checkList.count > 1 else {
+        print("Yes")
+        return
+    }
+    
+    
+    while checkList.last! == "0" {
+        checkList.popLast()
+    }
+
+    for i in 0..<(checkList.count / 2) {
+        if checkList[i] != checkList[checkList.count - i - 1] {
+            print("No")
+            return
+        }
+    }
+    print("Yes")
+
+    
+}
+
+Palindrome0()
