ab401b88a6045cb8a4c17f0a0d543c055325dedd
Yosuke Nakayama <ihatenonoildressing@gmail.com>
2021-07-05 06:34:56 +0900

    update

---
+++ 2021July/2021July/Thinking/main.swift
@@ -0,0 +1,27 @@
+//
+//  Savings.swift
+//  2021July
+//
+//  Created by Yosuke Nakayama on 2021/07/05.
+//
+
+import Foundation
+
+func Savings() {
+    let N = Int(readLine()!)!
+    var sum = 0
+    var day = 1
+    
+    while true {
+        sum += day
+        day += 1
+        if N <= sum {
+            print(day-1)
+            return
+        }
+    }
+
+    
+}
+
+Savings()
