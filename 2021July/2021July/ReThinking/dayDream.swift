ae7539b9a2e75ebb3a60a807d4c9d3190d136cdb
Yosuke Nakayama <ihatenonoildressing@gmail.com>
2021-07-08 08:28:39 +0900

    update

--- 2021July/2021July/ReThinking/dayDream.swift
+++ 2021July/2021July/ReThinking/dayDream.swift
@@ -11,5 +11,37 @@ import Foundation
 
 func dayDream() {
     let n = readLine()!
+    
+    let dayDreamlist = ["dream", "dreamer", "erase", "eraser"]
+
+    for i in dayDreamlist {
+        print(i, Array(i).last!)
+        if n.last! == Array(i).last! {
+            print(i)
+            
+            // 最後尾の文字列を削除できるかどうか canClipLastWord
+            if true {
+                
+            } else {
+                
+            }
+        }
+    }
+    
+    func canClipLast(word: String, by: String) {
+        
+        var checkWord = word
+        var byWord = by
+        if by.last! == checkWord.last! {
+            checkWord.removeLast()
+            byWord.removeLast()
+            canClipLast(word: checkWord, by: byWord)
+        }
+        
+        
+    }
+
 
 }
+
+dayDream()
