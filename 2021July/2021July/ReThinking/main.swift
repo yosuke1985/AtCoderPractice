728dabe96a48f39c9643e2cc9666b9b622b88f0a
Yosuke Nakayama <ihatenonoildressing@gmail.com>
2021-07-22 10:10:58 +0900

    update

--- 2021July/2021July/ReThinking/main.swift
+++ 2021July/2021July/ReThinking/main.swift
@@ -1,45 +1,48 @@
 //
-//  ZeroSumRanges.swift
+//  zerosumrange.swift
 //  2021July
 //
-//  Created by Yosuke Nakayama on 2021/07/21.
+//  Created by Yosuke Nakayama on 2021/07/22.
 //
 
 import Foundation
 
-func ZeroSumRanges() {
-    let N = Int(readLine()!)!
-    let aList = readLine()!.split(separator: " ").map { Int($0)! }
-
-    var sumList: [Int] = [0]
-    var result = 0
-
-    print(N)
-    print(aList)
-    
-    for (index, element) in aList.enumerated() {
-        sumList.append(element+sumList[index])
+func solve(_ N:Int, _ A:[Int]) {
+    var count = [Int: Int]()
+    var answer = 0
+    var sum = 0
+    count[0, default: 0] += 1
+    for a in A {
+        sum += a
+        answer += count[sum, default: 0]
+        count[sum, default: 0] += 1
     }
-    
-    print(sumList)
-    
-    for i in 2...sumList.count {
-        var start = 0
-        let range = start..<(start+i)
-        
-        while start+i <= sumList.count {
-            if sumList[range].reduce(0, +) == 0 {
-                result += 1
-            }
-            
-            start += 1
-        }
-        
-        
-    }
-    
-    print("result", result)
-    
+    print(answer)
 }
 
-ZeroSumRanges()
+func zerosumrange() {
+    var tokenIndex = 0, tokenBuffer = [String]()
+    func readString() -> String {
+        if tokenIndex >= tokenBuffer.count {
+            tokenIndex = 0
+            tokenBuffer = readLine()!.split(separator: " ").map { String($0) }
+        }
+        defer { tokenIndex += 1 }
+        return tokenBuffer[tokenIndex]
+    }
+    func readInt() -> Int { Int(readString())! }
+    func readDouble() -> Double { Double(readString())! }
+    let N = readInt()
+    var A = [Int]()
+    for _ in 0..<N {
+        A.append(readInt())
+    }
+    solve(N, A)
+}
+
+#if DEBUG
+let caseNumber = 1
+_ = freopen("in_\(caseNumber).txt", "r", stdin)
+#endif
+
+zerosumrange()
