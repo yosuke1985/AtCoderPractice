dd3c0aa448cb548e3f61e56a15dda697652fe846
Yosuke Nakayama <ihatenonoildressing@gmail.com>
2021-09-09 10:02:27 +0900

    update

---
+++ 2021Sep/2021Sep/RemoveIt.swift
@@ -0,0 +1,27 @@
+//
+//  RemoveIt.swift
+//  RemoveIt
+//
+//  Created by Yosuke Nakayama on 2021/09/09.
+//
+
+import Foundation
+
+func RemoveIt() {
+    let nxList = readLine()!.split(separator: " ").map { Int($0)! }
+    var aList = readLine()!.split(separator: " ").map { Int($0)! }
+
+    let X = nxList[1]
+    
+    aList = aList.compactMap({ num in
+        num == X ? nil : num
+    })
+    
+    let result = aList.reduce(into: "") { result, num in
+        result += "\(num) "
+    }
+    print(result)
+    
+}
+
+RemoveIt()
