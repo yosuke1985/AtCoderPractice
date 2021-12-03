606218e1f0683f71052d07f35992fe8b9ce674c5
Yosuke <ihatenonoildressing@gmail.com>
2021-12-03 23:54:05 +0900

    update

---
+++ 2021Nov/2021Nov/KaprekarNumber.swift
@@ -0,0 +1,15 @@
+//
+//  KaprekarNumber.swift
+//  2021Nov
+//
+//  Created by yosuke.nakayama on 2021/12/03.
+//
+// https://atcoder.jp/contests/abc192/tasks/abc192_c
+
+import Foundation
+
+func KaprekarNumber() {
+    
+}
+
+//KaprekarNumber()
